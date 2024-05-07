321e067a2505a90c9ac1f630fe2ec2aa78c919c8
byulcode <bybit0408@gmail.com>
2024-05-07 20:59:23 +0900

    Title: 정수 사각형 최대 합, Time: 311ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240507/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	static int n;
+	static int[][] map, dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		map = new int[n][n];
+		dp = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			map[i] = Arrays.stream(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt)
+				.toArray();
+		}
+		dp[0][0] = map[0][0];
+
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = dp[i - 1][0] + map[i][0];
+			dp[0][i] = dp[0][i - 1] + map[0][i];
+		}
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + map[i][j];
+			}
+		}
+
+		System.out.println(dp[n - 1][n - 1]);
+	}
+}
